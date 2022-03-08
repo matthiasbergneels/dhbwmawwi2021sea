c66767078589abe7a5d48847748cdc8cc715d599
matthiasbergneels <matthiasuweberg@icloud.com>
2022-03-08 19:06:52 +0100

    Equals Implementation

--- src/lecture/chapter9/FullName.java
+++ src/lecture/chapter9/FullName.java
@@ -1,6 +1,6 @@
 package lecture.chapter9;
 
-public class FullName {
+public class FullName implements Comparable<FullName> {
 
   private String name;
   private String familyName;
@@ -14,4 +14,43 @@ public class FullName {
   public String toString() {
     return familyName + ", " + name;
   }
+
+  @Override
+  public int compareTo(FullName o) {
+
+    if(!this.familyName.equals(o.familyName)){
+      return this.familyName.compareTo(o.familyName);
+    }
+
+    return this.name.compareTo(o.name);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+
+    if(this == o){
+      return true;
+    }
+
+    if(o == null){
+      return false;
+    }
+
+    if(this.getClass() != o.getClass()){
+      return false;
+    }
+
+    FullName nameToCheck = (FullName)o;
+
+    if(!this.familyName.equals(nameToCheck.familyName)){
+      return false;
+    }
+
+    if(!this.name.equals(nameToCheck.name)){
+      return false;
+    }
+
+    return true;
+  }
+
 }
--- src/lecture/chapter9/SetExample.java
+++ src/lecture/chapter9/SetExample.java
@@ -1,5 +1,6 @@
 package lecture.chapter9;
 
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
@@ -39,6 +40,36 @@ public class SetExample {
       String name = i.next();
 
       System.out.println(name);
+    }
+
+
+    System.out.println("Eigene Objekte verwalten: ");
+
+    Set<FullName> setOfFullNames = new TreeSet<FullName>();
+
+    FullName zeusOne = new FullName("Zeus", "Göttlich");
+    FullName zeusTwo = new FullName("Zeus", "Göttlich");
+    FullName zeusThree = zeusOne;
+
+    System.out.println("ZeusOne equals ZeusTwo? " + zeusOne.equals(zeusTwo));
+    System.out.println("ZeusOne ==  ZeusTwo? " + (zeusOne == zeusTwo));
+    System.out.println("ZeusOne equals ZeusThree? " + zeusOne.equals(zeusThree));
+    System.out.println("ZeusOne ==  ZeusThree? " + (zeusOne == zeusThree));
+
+    System.out.println("ZeusOne hashCode(): " + zeusOne.hashCode());
+    System.out.println("ZeusTwo hashCode(): " + zeusTwo.hashCode());
+    System.out.println("ZeusThree hashCode(): " + zeusThree.hashCode());
+
+    setOfFullNames.add(new FullName("Volker", "Schmidt"));
+    setOfFullNames.add(new FullName("Gabi", "Michels"));
+    setOfFullNames.add(zeusOne);
+    setOfFullNames.add(new FullName("Franz", "Michels"));
+    setOfFullNames.add(zeusTwo);
+    setOfFullNames.add(zeusOne);
+
+
+    for(FullName currentName : setOfFullNames){
+      System.out.println(currentName);
     }
 
   }

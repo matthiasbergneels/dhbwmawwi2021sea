160e2682cf9d2d3e2b5f3812e08cbbe42e179443
Matthias Berg-Neels <matthiasuweberg@icloud.com>
2022-04-19 18:27:41 +0200

    LinkedWordList enhancement

--- src/lecture/chapter12/LinkedWordList.java
+++ src/lecture/chapter12/LinkedWordList.java
@@ -3,9 +3,11 @@ package lecture.chapter12;
 public class LinkedWordList {
 
   private WordNode firstWordNode = null;
+  private int size = 0;
 
   public void add(String word){
     WordNode newWordNode = new WordNode(word);
+    size++;
 
     if(firstWordNode == null){
       // Liste ist leer
@@ -22,22 +24,82 @@ public class LinkedWordList {
   }
 
 public boolean contains(String word){
+
+    /*
+    if(firstWordNode == null){
+      return false;
+    }
+
+    WordNode currentWordNode = firstWordNode;
+    if(word.equals(currentWordNode.getWord())){
+      return true;
+    }
+
+    while(currentWordNode.getNextWordNode() != null){
+      currentWordNode = currentWordNode.getNextWordNode();
+      if(word.equals(currentWordNode.getWord())){
+        return true;
+      }
+    }
+    */
+
+    WordNode currentWordNode = firstWordNode;
+    while(currentWordNode != null){
+      if(word.equals(currentWordNode.getWord())){
+        return true;
+      }
+      currentWordNode = currentWordNode.getNextWordNode();
+    }
+
     return false;
 }
 
 public int size(){
-    return 0;
+    return this.size;
 }
 
 public void printWordList(){
+  if(firstWordNode == null){
+    System.out.println("Liste ist leer");
+    return;
+  }
 
+  System.out.println("Listeninhalt: ");
+  printWordList(firstWordNode);
+}
+
+// Rekursive Ausgabe der Liste
+private void printWordList(WordNode wordNode){
+  System.out.println("- " + wordNode.getWord());
+  if(wordNode.getNextWordNode() != null){
+    printWordList(wordNode.getNextWordNode());
+  }
 }
 
 public boolean remove(String word){
+
+    if(firstWordNode != null){
+
+      if(firstWordNode.getWord().equals(word)){
+        firstWordNode = firstWordNode.getNextWordNode();
+        size--;
+        return true;
+      }
+
+      WordNode currentNode = firstWordNode;
+      while(currentNode.getNextWordNode() != null){
+        if(currentNode.getNextWordNode().getWord().equals(word)){
+          currentNode.setNextWordNode(currentNode.getNextWordNode().getNextWordNode());
+          size--;
+          return true;
+        }
+        currentNode = currentNode.getNextWordNode();
+      }
+
+    }
+
     return false;
 }
-
-
 
   private class WordNode{
     private String word;
@@ -57,7 +119,7 @@ public boolean remove(String word){
     }
 
     public String getWord(){
-      return this.getWord();
+      return this.word;
     }
   }
 
--- src/lecture/chapter12/LinkedWordListRun.java
+++ src/lecture/chapter12/LinkedWordListRun.java
@@ -6,12 +6,32 @@ public class LinkedWordListRun {
 
     LinkedWordList myWords = new LinkedWordList();
 
+    System.out.println("Anzahl Element: " + myWords.size());
+
+    System.out.println("Enthält Kabelbruch: " + myWords.contains("Kabelbruch"));
+
+    myWords.add("Kabelbruch");
+    myWords.add("Sesambrot");
+    myWords.add("Schimmelkruste");
+    myWords.add("Kabelbruch");
+    myWords.add("Sesambrot");
+    myWords.add("Schimmelkruste");
     myWords.add("Kabelbruch");
     myWords.add("Sesambrot");
     myWords.add("Schimmelkruste");
 
+    System.out.println("Enthält Kabelbruch: " + myWords.contains("Kabelbruch"));
+    System.out.println("Enthält Käsekuchen: " + myWords.contains("Käsekuchen"));
 
-    System.out.println("Liste ausgeben:");
+    System.out.println("Anzahl Element: " + myWords.size());
+
+    myWords.remove("Kabelbruch");
+    myWords.remove("Schimmelkruste");
+    myWords.remove("Himmelbett");
+
+    System.out.println("Anzahl Element: " + myWords.size());
+
+    myWords.printWordList();
 
 
   }
